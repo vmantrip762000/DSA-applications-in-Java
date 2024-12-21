dd4755f455f1ba81b6d0a46d9f22c6c83402a3fa
Mantri Aditya <159584214+vmantrip762000@users.noreply.github.com>
2024-12-20 18:27:53 -0600

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Operators {
+
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println("a" + "b");
+        System.out.println("a" + 1);
+        System.out.println(new ArrayList<>() + "Aditya");
+        //System.out.println(new Integer(53) + new ArrayList<>());
+        // + operator is only defined for primitives and complex objects give
+    }
+
+
+
+}
